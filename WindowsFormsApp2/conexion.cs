0edd00a319b1d304b4955baedd460f326e524655
erika <erika.fernandezg@udea.edu.co>
2019-05-21 13:17:26 -0500

    Entrega final del curso , software II de gesis

---
+++ WindowsFormsApp2/conexion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    class conexion
+    {
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+
+        public conexion()
+        {
+            try
+            {
+
+                cn = new SqlConnection("Data Source=SALA235-PC05SQLEXPRESS;Initial Catalog=ConsultorioSonrrisaPerfecta;User ID=sa;Password=***********");
+                cn.Open();
+                MessageBox.Show("Conectado");
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("No se conecto con la base de datos: " +ex.ToString());
+            }
+
+    }
+
+        }
+
+
+
+
+
+    }
+
+    
+    
+    
+
+    
+    
+    
+
+
+
+
+
